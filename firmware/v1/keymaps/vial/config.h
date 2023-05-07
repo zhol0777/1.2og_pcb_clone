36cd836b4721d701f0ad64c0735c04dadc3c1b98
Zhol Internet <zhol@internet.keyboard>
2023-05-07 03:21:45 -0700

    firmware

---
+++ firmware/v1/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xEF, 0x9B, 0x89, 0xDF, 0x5C, 0x5E, 0xCA, 0xC5}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
